f073bb2c0ca15ec791448c0b7e12e2ac4fb4128e
Mustafa Cihat YAPICI <57036952+mustafacihat@users.noreply.github.com>
2021-03-21 09:55:29 +0100

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,22 @@
+package eu5Studies;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		
+		
+		int currentYear=2021;
+		
+		
+		int birthYear=1990;
+		
+		int age = currentYear - birthYear;
+	
+		int phone = 0b0111;
+		
+		System.out.println(phone);
+	
+
+	}
+
+}
